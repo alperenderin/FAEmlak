96df8e19ae5c86fd7444b61ae8636d9953afd13b
alperenderin <alperenderinn@gmail.com>
2020-12-25 20:08:07 +0300

    Business katmanında işlemler yapıldı.

---
+++ FaEmlak.Business/Abstract/IPropertyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using FAEmlak.Entity;
+
+namespace FAEmlak.Business.Abstract
+{
+    public interface IPropertyService
+    {
+        void Create(Property entity);
+        void Update(Property entity);
+        void Delete(Property deneme);
+        Property GetById(int id);
+        List<Property> GetProperties();
+        List<Property> GetPropertiesByTypeAndCategory(PropertyType propertyType, PropertyCategory propertyCategory);
+    }
+}
---
+++ FaEmlak.Business/Concrete/PropertyService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using FAEmlak.Business.Abstract;
+using FAEmlak.Data.Abstract;
+using FAEmlak.Entity;
+
+namespace FAEmlak.Business.Concrete
+{
+    public class PropertyService : IPropertyService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PropertyService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Create(Property entity)
+        {
+            _unitOfWork.Properties.Create(entity);
+        }
+
+        public void Update(Property entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Property entity)
+        {
+            _unitOfWork.Properties.Delete(entity);
+        }
+
+        public Property GetById(int id)
+        {
+            return _unitOfWork.Properties.GetById(id);
+        }
+
+        public List<Property> GetProperties()
+        {
+            return _unitOfWork.Properties.GetProperties();
+        }
+
+        public List<Property> GetPropertiesByTypeAndCategory(PropertyType propertyType, PropertyCategory propertyCategory)
+        {
+            return _unitOfWork.Properties.GetPropertiesByTypeAndCategory(propertyType, propertyCategory);
+        }
+    }
+}
