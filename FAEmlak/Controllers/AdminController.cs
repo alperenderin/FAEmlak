8abb2d8dc4d75de0954aeeccd0475a2925c90899
Furkan Ergün <furkan.ergun@ogr.sakarya.edu.tr>
2020-12-29 22:24:45 +0300

    Admin panelinde Roller, Üyeler ve İlanlar listelendi.

--- FAEmlak/Controllers/AdminController.cs
+++ FAEmlak/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FAEmlak.Business.Abstract;
+using FAEmlak.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -8,13 +10,22 @@ using System.Threading.Tasks;
 
 namespace FAEmlak.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private RoleManager<IdentityRole> _roleManager;
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        private IPropertyService _propertyService;
+        private UserManager<User> _userManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager,
+            IPropertyService propertyService,
+            UserManager<User> userManager)
         {
             _roleManager = roleManager;
+            _propertyService = propertyService;
+            _userManager = userManager;
         }
+
         public IActionResult Index()
         {
             return View();
@@ -25,6 +36,20 @@ namespace FAEmlak.Controllers
         {
             var roles = _roleManager.Roles.ToList();
             return View(roles);
+        }
+
+        [Route("[controller]/Properties")]
+        public IActionResult PropertiesList()
+        {
+            var properties = _propertyService.GetProperties();
+            return View(properties);
+        }
+
+        [Route("[controller]/Users")]
+        public IActionResult UserList()
+        {
+            var users = _userManager.Users.ToList() ;
+            return View(users);
         }
 
     }
